f71d0afecc46461e7763b193a320e1b95fa5cffb
Jeroen <Jeroenbruinsma@gmail.com>
2020-10-29 09:38:55 +0100

    expand menu for today

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 4000;
+// const User = require("../models").user;
 
-// const User = require("./models").user;
 
 app.get("/", (req, res) => {
-    res.send("Good morning #46");
-  });
+  res.send("Good morning #46");
+});
 app.get("/menu", (req, res) => {
-    res.send("##### MENU FOR TODAY ##### <br><ul><li>Breakfast: Say hello & recap about Sequelize-cli, models, migrations </li><li>Lunch(13:30): JWT's bcrypt & login </li><li>Dinner(16:00): auth in middleware &  </li><li>Tomorrow: 😭 PROJECT TIME!!!!  </li></ul>");
-  });
+  res.send(
+    "##### MENU FOR TODAY ##### <br><ul><li>Breakfast: Say hello and show my app, recap about Sequelize-cli, models, migrations </li><li>Lunch(13:30): JWT's bcrypt & login & routers </li><li>Dinner(16:00): auth in middleware, pagination  </li><li>Tomorrow: 😭 PROJECT TIME!!!!  </li></ul>"
+  );
+});
 
 app.use(express.json());
-app.listen(PORT, ()=> console.log("The server is Alive on port", PORT ))
+app.listen(PORT, () => console.log("The server is Alive on port", PORT));
